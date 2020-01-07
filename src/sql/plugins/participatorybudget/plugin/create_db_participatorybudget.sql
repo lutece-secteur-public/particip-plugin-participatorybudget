81a8948c6702d8115b9410e66e38ab723e81a27c
Guillaume Lépine <guillaume.lepine@sogeti.com>
2020-01-07 09:48:40 +0100

    PARTBUDG-40 : Corrects sql script for HSQL

--- src/sql/plugins/participatorybudget/plugin/create_db_participatorybudget.sql
+++ src/sql/plugins/participatorybudget/plugin/create_db_participatorybudget.sql
@@ -1,184 +1,175 @@
-DROP TABLE IF EXISTS `task_notify_documentbp_cf`;
-DROP TABLE IF EXISTS `participatorybudget_campaign_area`;
-DROP TABLE IF EXISTS `participatorybudget_votes_per_location`;
-DROP TABLE IF EXISTS `participatorybudget_votes_history`;
-DROP TABLE IF EXISTS `participatorybudget_votes`;
-DROP TABLE IF EXISTS `participatorybudget_user_access_vote`;
-DROP TABLE IF EXISTS `participatorybudget_rgpd_treatment_log`;
-DROP TABLE IF EXISTS `participatorybudget_campaign_theme`;
-DROP TABLE IF EXISTS `participatorybudget_campaign_phase`;
-DROP TABLE IF EXISTS `participatorybudget_campaign_phase_type`;
-DROP TABLE IF EXISTS `participatorybudget_campaign_image`;
-DROP TABLE IF EXISTS `participatorybudget_campaign`;
-DROP TABLE IF EXISTS `participatorybudget_campaign_moderation_type`;
-DROP TABLE IF EXISTS `participatorybudget_bizstat_file`;
+DROP TABLE IF EXISTS task_notify_documentbp_cf;
+DROP TABLE IF EXISTS participatorybudget_campaign_area;
+DROP TABLE IF EXISTS participatorybudget_votes_per_location;
+DROP TABLE IF EXISTS participatorybudget_votes_history;
+DROP TABLE IF EXISTS participatorybudget_votes;
+DROP TABLE IF EXISTS participatorybudget_user_access_vote;
+DROP TABLE IF EXISTS participatorybudget_rgpd_treatment_log;
+DROP TABLE IF EXISTS participatorybudget_campaign_theme;
+DROP TABLE IF EXISTS participatorybudget_campaign_phase;
+DROP TABLE IF EXISTS participatorybudget_campaign_phase_type;
+DROP TABLE IF EXISTS participatorybudget_campaign_image;
+DROP TABLE IF EXISTS participatorybudget_campaign;
+DROP TABLE IF EXISTS participatorybudget_campaign_moderation_type;
+DROP TABLE IF EXISTS participatorybudget_bizstat_file;
 
-CREATE TABLE IF NOT EXISTS `participatorybudget_bizstat_file` (
-  `id_bizstat_file` int(11) NOT NULL AUTO_INCREMENT,
-  `status` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `id_admin_user` int(11) NOT NULL,
-  `admin_user_access_code` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `admin_user_email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `reason` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `file_name` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
-  `description` varchar(1000) COLLATE utf8_unicode_ci NOT NULL,
-  `error` varchar(4000) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `file_value` longblob,
-  PRIMARY KEY (`id_bizstat_file`)
+CREATE TABLE IF NOT EXISTS participatorybudget_bizstat_file (
+  id_bizstat_file int NOT NULL AUTO_INCREMENT,
+  status varchar(50) NOT NULL,
+  id_admin_user int NOT NULL,
+  admin_user_access_code varchar(255) NOT NULL,
+  admin_user_email varchar(255) NOT NULL,
+  reason varchar(255) NOT NULL,
+  file_name varchar(500) NOT NULL,
+  description varchar(1000) NOT NULL,
+  error varchar(4000) DEFAULT NULL,
+  creation_date timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  file_value LONG VARBINARY,
+  PRIMARY KEY (id_bizstat_file)
 );
 
-CREATE TABLE IF NOT EXISTS `participatorybudget_campaign_moderation_type` (
-  `id_moderation_type` int(6) NOT NULL,
-  `code_moderation_type` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `libelle` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id_moderation_type`),
-  UNIQUE KEY `code_moderation_type` (`code_moderation_type`)
+CREATE TABLE IF NOT EXISTS participatorybudget_campaign_moderation_type (
+  id_moderation_type int NOT NULL,
+  code_moderation_type varchar(50) NOT NULL,
+  libelle varchar(255) NOT NULL,
+  PRIMARY KEY (id_moderation_type)
+);
+ALTER TABLE participatorybudget_campaign_moderation_type ADD CONSTRAINT uc_code_moderation_type UNIQUE (code_moderation_type);
+
+CREATE TABLE IF NOT EXISTS participatorybudget_campaign (
+  id_campagne int NOT NULL,
+  code_campagne varchar(50) NOT NULL,
+  title varchar(255) NOT NULL,
+  description varchar(1000) NOT NULL,
+  active smallint NOT NULL,
+  code_moderation_type varchar(50) NOT NULL,
+  moderation_duration int DEFAULT NULL,
+  PRIMARY KEY (id_campagne)
+);
+ALTER TABLE participatorybudget_campaign ADD CONSTRAINT uc_code_campagne UNIQUE (code_campagne);
+ALTER TABLE participatorybudget_campaign ADD CONSTRAINT fk_participatorybudget_campaign_moderation  FOREIGN KEY (code_moderation_type) REFERENCES participatorybudget_campaign_moderation_type (code_moderation_type);
+
+CREATE TABLE IF NOT EXISTS participatorybudget_campaign_image (
+  id_campagne_image int NOT NULL,
+  code_campagne varchar(50) NOT NULL,
+  id_file int NOT NULL,
+  PRIMARY KEY (id_campagne_image)
+);
+ALTER TABLE participatorybudget_campaign_image ADD CONSTRAINT fk_participatorybudget_campaign_images_campagne  FOREIGN KEY (code_campagne) REFERENCES participatorybudget_campaign (code_campagne);
+ALTER TABLE participatorybudget_campaign_image ADD CONSTRAINT fk_participatorybudget_campaign_images_file  FOREIGN KEY (id_file) REFERENCES core_file (id_file);
+
+CREATE TABLE IF NOT EXISTS participatorybudget_campaign_phase_type (
+  id_phase_type int NOT NULL,
+  code_phase_type varchar(50) NOT NULL,
+  libelle varchar(255) NOT NULL,
+  PRIMARY KEY (id_phase_type)
+);
+ALTER TABLE participatorybudget_campaign_phase_type ADD CONSTRAINT uc_code_phase_type UNIQUE (code_phase_type);
+
+CREATE TABLE IF NOT EXISTS participatorybudget_campaign_phase (
+  id_campagne_phase int NOT NULL,
+  code_phase_type varchar(50) NOT NULL,
+  code_campagne varchar(50) NOT NULL,
+  start datetime NOT NULL,
+  end datetime NOT NULL,
+  PRIMARY KEY (id_campagne_phase)
+);
+ALTER TABLE participatorybudget_campaign_phase ADD CONSTRAINT fk_participatorybudget_campaign_phases_campagne  FOREIGN KEY (code_campagne) REFERENCES participatorybudget_campaign (code_campagne);
+ALTER TABLE participatorybudget_campaign_phase ADD CONSTRAINT fk_participatorybudget_campaign_phases_phase  FOREIGN KEY (code_phase_type) REFERENCES participatorybudget_campaign_phase_type (code_phase_type);
+
+CREATE TABLE IF NOT EXISTS participatorybudget_campaign_theme (
+  id_campagne_theme int NOT NULL,
+  code_campagne varchar(50) NOT NULL,
+  code_theme varchar(50) NOT NULL,
+  title varchar(255) NOT NULL,
+  description varchar(1000) NOT NULL,
+  active smallint NOT NULL,
+  image_file int DEFAULT NULL,
+  PRIMARY KEY (id_campagne_theme)
+);
+ALTER TABLE participatorybudget_campaign_theme ADD CONSTRAINT uc_code_theme UNIQUE (code_campagne,code_theme);
+ALTER TABLE participatorybudget_campaign_theme ADD CONSTRAINT fk_participatorybudget_campaign_themes_campagne  FOREIGN KEY (code_campagne) REFERENCES participatorybudget_campaign (code_campagne);
+
+CREATE TABLE IF NOT EXISTS participatorybudget_rgpd_treatment_log (
+  id_treatment_log int NOT NULL AUTO_INCREMENT,
+  treatment_timestamp timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  id_admin_user int NOT NULL,
+  admin_user_access_code varchar(255) NOT NULL,
+  admin_user_email varchar(255) NOT NULL,
+  treatment_type varchar(30) NOT NULL,
+  treatment_object_name varchar(100) NOT NULL,
+  treatment_object_fields varchar(1000) NOT NULL,
+  PRIMARY KEY (id_treatment_log)
 );
 
-CREATE TABLE IF NOT EXISTS `participatorybudget_campaign` (
-  `id_campagne` int(6) NOT NULL,
-  `code_campagne` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL,
-  `active` smallint(6) NOT NULL,
-  `code_moderation_type` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `moderation_duration` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_campagne`),
-  UNIQUE KEY `code_campagne` (`code_campagne`),
-  KEY `fk_participatorybudget_campaign_moderation` (`code_moderation_type`),
-  CONSTRAINT `fk_participatorybudget_campaign_moderation` FOREIGN KEY (`code_moderation_type`) REFERENCES `participatorybudget_campaign_moderation_type` (`code_moderation_type`)
+CREATE TABLE IF NOT EXISTS participatorybudget_user_access_vote (
+  id_user varchar(255) NOT NULL,
+  has_acces_vote int NOT NULL,
+  PRIMARY KEY (id_user)
 );
 
-/* This line to avoid error on constraint with core_file 
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-CREATE TABLE IF NOT EXISTS `participatorybudget_campaign_image` (
-  `id_campagne_image` int(6) NOT NULL,
-  `code_campagne` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `id_file` int(11) NOT NULL,
-  PRIMARY KEY (`id_campagne_image`),
-  KEY `code_campagne` (`code_campagne`,`id_file`),
-  KEY `id_file` (`id_file`),
-  CONSTRAINT `fk_participatorybudget_campaign_images_campagne` FOREIGN KEY (`code_campagne`) REFERENCES `participatorybudget_campaign` (`code_campagne`),
-  CONSTRAINT `fk_participatorybudget_campaign_images_file` FOREIGN KEY (`id_file`) REFERENCES `core_file` (`id_file`)
-);
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-
-CREATE TABLE IF NOT EXISTS `participatorybudget_campaign_phase_type` (
-  `id_phase_type` int(6) NOT NULL,
-  `code_phase_type` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `libelle` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id_phase_type`),
-  UNIQUE KEY `code_phase_type` (`code_phase_type`)
+CREATE TABLE IF NOT EXISTS participatorybudget_votes (
+  id_user varchar(255) NOT NULL,
+  id_projet int NOT NULL,
+  date_vote timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+  arrondissement int NOT NULL,
+  age int NOT NULL,
+  birth_date varchar(255) DEFAULT NULL,
+  ip_address varchar(100) DEFAULT '' NOT NULL,
+  title varchar(255) DEFAULT '' NOT NULL,
+  localisation int NOT NULL,
+  thematique varchar(255) DEFAULT '' NOT NULL,
+  status int DEFAULT '0' NOT NULL,
+  mobile_phone varchar(50) DEFAULT NULL,
+  email varchar(500) DEFAULT NULL,
+  PRIMARY KEY (id_user,id_projet)
 );
 
-CREATE TABLE IF NOT EXISTS `participatorybudget_campaign_phase` (
-  `id_campagne_phase` int(6) NOT NULL,
-  `code_phase_type` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `code_campagne` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `start` datetime NOT NULL,
-  `end` datetime NOT NULL,
-  PRIMARY KEY (`id_campagne_phase`),
-  KEY `code_campagne` (`code_campagne`,`code_phase_type`),
-  KEY `fk_participatorybudget_campaign_phases_phase` (`code_phase_type`),
-  CONSTRAINT `fk_participatorybudget_campaign_phases_campagne` FOREIGN KEY (`code_campagne`) REFERENCES `participatorybudget_campaign` (`code_campagne`),
-  CONSTRAINT `fk_participatorybudget_campaign_phases_phase` FOREIGN KEY (`code_phase_type`) REFERENCES `participatorybudget_campaign_phase_type` (`code_phase_type`)
+CREATE TABLE IF NOT EXISTS participatorybudget_votes_history (
+  id_user varchar(255) NOT NULL,
+  id_projet int NOT NULL,
+  date_vote timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+  arrondissement int NOT NULL,
+  age int NOT NULL,
+  birth_date varchar(255) DEFAULT NULL,
+  ip_address varchar(100) DEFAULT '' NOT NULL,
+  title varchar(255) DEFAULT '' NOT NULL,
+  localisation int NOT NULL,
+  thematique varchar(255) DEFAULT '' NOT NULL,
+  status int NOT NULL,
+  id int NOT NULL,
+  status_export_stats int DEFAULT '0' NOT NULL,
+  PRIMARY KEY (id)
+);
+CREATE INDEX participatorybudget_votes_history_index_id_user ON participatorybudget_votes_history ( id_user );
+
+CREATE TABLE IF NOT EXISTS participatorybudget_votes_per_location (
+  id int NOT NULL,
+  localisation_ardt varchar(50) NOT NULL,
+  nb_votes int NOT NULL,
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS `participatorybudget_campaign_theme` (
-  `id_campagne_theme` int(6) NOT NULL,
-  `code_campagne` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `code_theme` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL,
-  `active` smallint(6) NOT NULL,
-  `image_file` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_campagne_theme`),
-  UNIQUE KEY `code_campagne` (`code_campagne`,`code_theme`),
-  CONSTRAINT `participatorybudget_campaign_themes_campagne` FOREIGN KEY (`code_campagne`) REFERENCES `participatorybudget_campaign` (`code_campagne`)
+CREATE TABLE participatorybudget_campaign_area (
+  id_campagne_area int NOT NULL,
+  code_campagne varchar(50) NOT NULL,
+  title varchar(255) NOT NULL,
+  active smallint NOT NULL,
+  type varchar(50) NOT NULL,
+  number_votes int NOT NULL,
+  PRIMARY KEY (id_campagne_area)
 );
+ALTER TABLE participatorybudget_campaign_area ADD CONSTRAINT fk_participatorybudget_campaign_areas_campagne  FOREIGN KEY (code_campagne) REFERENCES participatorybudget_campaign (code_campagne) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-CREATE TABLE IF NOT EXISTS `participatorybudget_rgpd_treatment_log` (
-  `id_treatment_log` int(11) NOT NULL AUTO_INCREMENT,
-  `treatment_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `id_admin_user` int(11) NOT NULL,
-  `admin_user_access_code` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `admin_user_email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `treatment_type` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `treatment_object_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `treatment_object_fields` varchar(1000) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id_treatment_log`),
-  KEY `id_treatment_log_index` (`id_treatment_log`)
-);
 
-CREATE TABLE IF NOT EXISTS `participatorybudget_user_access_vote` (
-  `id_user` varchar(255) NOT NULL,
-  `has_acces_vote` int(11) NOT NULL,
-  PRIMARY KEY (`id_user`)
-);
-
-CREATE TABLE IF NOT EXISTS `participatorybudget_votes` (
-  `id_user` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `id_projet` int(11) NOT NULL,
-  `date_vote` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `arrondissement` int(11) NOT NULL,
-  `age` int(11) NOT NULL,
-  `birth_date` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `ip_address` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `localisation` int(11) NOT NULL,
-  `thematique` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `status` int(11) NOT NULL DEFAULT '0',
-  `mobile_phone` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `email` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id_user`,`id_projet`)
-);
-
-CREATE TABLE IF NOT EXISTS `participatorybudget_votes_history` (
-  `id_user` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `id_projet` int(11) NOT NULL,
-  `date_vote` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `arrondissement` int(11) NOT NULL,
-  `age` int(11) NOT NULL,
-  `birth_date` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `ip_address` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `localisation` int(11) NOT NULL,
-  `thematique` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `status` int(11) NOT NULL,
-  `id` int(11) NOT NULL,
-  `status_export_stats` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `participatorybudget_votes_history_index_id_user` (`id_user`)
-);
-
-CREATE TABLE IF NOT EXISTS `participatorybudget_votes_per_location` (
-  `id` int(11) NOT NULL,
-  `localisation_ardt` varchar(50) NOT NULL,
-  `nb_votes` int(10) NOT NULL,
-  PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `participatorybudget_campaign_area` (
-  `id_campagne_area` int(6) NOT NULL,
-  `code_campagne` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `active` smallint(6) NOT NULL,
-  `type` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `number_votes` int(6) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id_campagne_area`),
-  KEY `fk_participatorybudget_campaign_areas_campagne_idx` (`code_campagne`),
-  CONSTRAINT `fk_participatorybudget_campaign_areas_campagne` FOREIGN KEY (`code_campagne`) REFERENCES `participatorybudget_campaign` (`code_campagne`) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-
-CREATE TABLE IF NOT EXISTS `task_notify_documentbp_cf` (
-  `id_task` int(11) NOT NULL,
-  `sender_name` varchar(255) DEFAULT NULL,
-  `sender_email` varchar(255) DEFAULT NULL,
-  `subject` varchar(255) DEFAULT NULL,
-  `message` mediumtext,
-  `recipients_cc` varchar(255) NOT NULL DEFAULT '',
-  `recipients_bcc` varchar(255) NOT NULL DEFAULT '',
-  `is_abonnes` smallint(6) NOT NULL,
-  PRIMARY KEY (`id_task`)
+CREATE TABLE IF NOT EXISTS task_notify_documentbp_cf (
+  id_task int NOT NULL,
+  sender_name varchar(255) DEFAULT NULL,
+  sender_email varchar(255) DEFAULT NULL,
+  subject varchar(255) DEFAULT NULL,
+  message varchar(4000),
+  recipients_cc varchar(255) DEFAULT '' NOT NULL,
+  recipients_bcc varchar(255) DEFAULT '' NOT NULL,
+  is_abonnes smallint NOT NULL,
+  PRIMARY KEY (id_task)
 );
--- src/sql/plugins/participatorybudget/plugin/init_db_participatorybudget.sql
+++ src/sql/plugins/participatorybudget/plugin/init_db_participatorybudget.sql
@@ -1,12 +1,12 @@
-INSERT INTO `participatorybudget_campaign_moderation_type` (`id_moderation_type`, `code_moderation_type`, `libelle`) VALUES
+INSERT INTO participatorybudget_campaign_moderation_type (id_moderation_type, code_moderation_type, libelle) VALUES
 	(1, 'NONE', 'No moderation'),
 	(2, 'PUBLISH', 'Immediate publication'),
 	(3, 'WAIT', 'Delayed publication');
 
-INSERT INTO `participatorybudget_campaign` (`id_campagne`, `code_campagne`, `title`, `description`, `active`, `code_moderation_type`, `moderation_duration`) VALUES
+INSERT INTO participatorybudget_campaign (id_campagne, code_campagne, title, description, active, code_moderation_type, moderation_duration) VALUES
 	(1, 'A', 'Participatory Budget 2019', 'Participatory Budget 2019', 1, 'NONE', 0);
 
-INSERT INTO `participatorybudget_campaign_phase_type` (`id_phase_type`, `code_phase_type`, `libelle`) VALUES
+INSERT INTO participatorybudget_campaign_phase_type (id_phase_type, code_phase_type, libelle) VALUES
 	(1, 'PRE_IDEATION', 'Before ideation phase'),
 	(2, 'IDEATION', 'Ideation phase open'),
 	(3, 'POST_IDEATION', 'After ideation phase'),
@@ -19,7 +19,7 @@ INSERT INTO `participatorybudget_campaign_phase_type` (`id_phase_type`, `code_ph
 	(10, 'PRE_RESULT', 'Before result phase'),
 	(11, 'RESULT', 'Announce vote results');
 
-INSERT INTO `participatorybudget_campaign_phase` (`id_campagne_phase`, `code_phase_type`, `code_campagne`, `start`, `end`) VALUES
+INSERT INTO participatorybudget_campaign_phase (id_campagne_phase, code_phase_type, code_campagne, start, end) VALUES
 	(1, 'PRE_IDEATION', 'A', '2019-08-09 00:00:00', '2019-08-22 23:59:59'),
 	(2, 'IDEATION', 'A', '2019-08-23 00:00:00', '2019-12-05 23:59:59'),
 	(3, 'POST_IDEATION', 'A', '2019-09-06 00:00:00', '2019-09-19 23:59:59'),
@@ -32,13 +32,13 @@ INSERT INTO `participatorybudget_campaign_phase` (`id_campagne_phase`, `code_pha
 	(10, 'PRE_RESULT', 'A', '2019-12-13 00:00:00', '2019-12-26 23:59:59'),
 	(11, 'RESULT', 'A', '2019-12-27 00:00:00', '2020-01-09 23:59:59');
 
-INSERT INTO `participatorybudget_campaign_theme` (`id_campagne_theme`, `code_campagne`, `code_theme`, `title`, `description`, `active`, `image_file`) VALUES
+INSERT INTO participatorybudget_campaign_theme (id_campagne_theme, code_campagne, code_theme, title, description, active, image_file) VALUES
 	(1, 'A', 'ENVIRONMENT', 'Environment issues', 'Environment issues', 1, NULL),
 	(2, 'A', 'SOCIAL', 'Social issues', 'Social issues', 1, NULL),
 	(3, 'A', 'SPORT', 'Sport', 'Sport', 1, NULL),
 	(4, 'A', 'GENERAL', 'General', 'General', 1, NULL);
 
-INSERT INTO `participatorybudget_votes_per_location` (`id`, `localisation_ardt`, `nb_votes`) VALUES
+INSERT INTO participatorybudget_votes_per_location (id, localisation_ardt, nb_votes) VALUES
 	(1, 'whole_city', 2),
 	(2, 'area_1', 3),
 	(3, 'area_2', 0),
@@ -46,7 +46,7 @@ INSERT INTO `participatorybudget_votes_per_location` (`id`, `localisation_ardt`,
 	(5, 'area_4', 0),
 	(6, 'area_5', 1);
 
-INSERT INTO `participatorybudget_campaign_area` VALUES 
+INSERT INTO participatorybudget_campaign_area VALUES 
 	(0,'A','whole city',1,'whole','3'),
 	(1,'A','area 1',1,'localized','1'),
 	(2,'A','area 2',1,'localized','1'),
@@ -54,6 +54,6 @@ INSERT INTO `participatorybudget_campaign_area` VALUES
 	(4,'A','area 4',1,'localized','2'),
 	(5,'A','area 5',1,'localized','1');
 
-INSERT INTO `task_notify_documentbp_cf` (`id_task`, `sender_name`, `sender_email`, `subject`, `message`, `recipients_cc`, `recipients_bcc`, `is_abonnes`) VALUES
+INSERT INTO task_notify_documentbp_cf (id_task, sender_name, sender_email, subject, message, recipients_cc, recipients_bcc, is_abonnes) VALUES
 	(36, 'Ville de Paris - budget participatif', 'budget.participatif@paris.fr', 'Des nouvelles des projets gagnants !', '<p>Bonjour,</p>\r\n<p>Nous avons le plaisir de vous informer que la fiche du projet &laquo; ${titre_projet!} &raquo; auquel vous &ecirc;tes associ&eacute;&middot;e,&nbsp;a &eacute;t&eacute; mise &agrave; jour :</p>\r\n<blockquote><span style="color: #0000ff;">${message_notification!}</span></blockquote>\r\n<p>Vous pouvez consulter la fiche du projet en cliquant sur le lien suivant, pour voir notamment le d&eacute;tail du planning de mise en &oelig;uvre :</p>\r\n<blockquote><a href="${url_fiche!\'\'}">fiche du projet "${titre_projet!}"</a></blockquote>\r\n<p>Pour toute question, vous pouvez consulter <span style="text-decoration: underline;"><span style="color: #0000ff;"><a style="color: #0000ff; text-decoration: underline;" href="https://budgetparticipatif.paris.fr/bp/jsp/site/Portal.jsp?page=helpdesk&amp;faq_id=3" target="_blank">la foire aux questions du budget participatif</a></span></span> ou nous &eacute;crire &agrave; <span style="text-decoration: underline;"><span style="color: #0000ff;"><a style="color: #0000ff; text-decoration: underline;" href="mailto:budgetparticipatif@paris.fr" target="_blank">budgetparticipatif@paris.fr</a></span></span></p>\r\n<p>L&rsquo;&eacute;quipe du budget participatif de la Ville de Paris</p>\r\n<p><img src="https://budgetparticipatif.paris.fr/bp/document?id=5278&amp;id_attribute=43" alt="mdp" width="103" height="123" /></p>\r\n<p>#budgetparticipatif</p>\r\n<p>Nous suivre sur <span style="text-decoration: underline;"><span style="color: #0000ff;"><a style="color: #0000ff; text-decoration: underline;" href="https://www.facebook.com/groups/NotreBudget/" target="_blank">Facebook</a></span></span> et sur <span style="text-decoration: underline;"><span style="color: #0000ff;"><a style="color: #0000ff; text-decoration: underline;" href="https://twitter.com/Paris" target="_blank">Twitter</a></span></span></p>\r\n<p>Pour param&eacute;trer les notifications que vous recevez, rendez-vous dans la rubrique &laquo; Mes notifications &raquo; de votre compte sur budgetparticipatif.paris</p>', 'ddct-mpc-site-bp@paris.fr', '', 1);
 
